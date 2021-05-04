84333fa338218dc655716798665b0383246408ee
= <magdalenaanna.czajka@gmail.com>
2021-05-04 18:09:45 +0200

    Create app with sidebar and empty main part

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { Divider, Drawer, List, ListSubheader } from '@material-ui/core'
+import { SET_BREEDS_SUCCESS } from './rootReducer'
+import { selectListBreeds, selectLoading } from './selectors'
+import { useStyles } from './styles'
+import { SidebarListItem } from './components/SidebarListItem'
+import { quantitySkeletons } from './components/pluralSkeleton'
 
-function App() {
+function App () {
+  const classes = useStyles()
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    fetch('https://dog.ceo/api/breeds/list/all')
+      .then(res => res.json())
+      .then(data => dispatch({ type: SET_BREEDS_SUCCESS, payload: data }))
+  }, [])
+
+  const listBreeds = useSelector(selectListBreeds)
+  const loaded = useSelector(selectLoading)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className={classes.root}>
+      <Drawer
+        className={classes.drawer}
+        variant='persistent'
+        anchor='left'
+        open
+        classes={{
+          paper: classes.drawerPaper
+        }}
+      >
+        <List
+          component='nav'
+          aria-labelledby='nested-list-subheader'
+          subheader={
+            <ListSubheader component='div' id='nested-list-subheader'>
+              Breeds list:
+            </ListSubheader>
+          }
         >
-          Learn React
-        </a>
-      </header>
+          <Divider />
+          {loaded === false
+            ? quantitySkeletons()
+            : (
+                listBreeds.map(breed => (
+                  <List key={breed.name}>
+                    <SidebarListItem name={breed.name} subItems={breed.subBreeds} />
+                  </List>
+                ))
+              )}
+        </List>
+      </Drawer>
+      <main>
+        zdjęcia rasy lub komunikat
+      </main>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
