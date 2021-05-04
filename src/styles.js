44b5d1d51ba34814ec0630abb3f56f7b54103707
= <magdalenaanna.czajka@gmail.com>
2021-05-04 18:12:59 +0200

    Export styles to new file and add Material UI and Ramda to project

---
+++ src/styles.js
@@ -0,0 +1,28 @@
+import { makeStyles } from '@material-ui/core/styles'
+
+const drawerWidth = 240
+
+export const useStyles = makeStyles((theme) => {
+  return {
+    root: {
+      display: 'flex',
+      color: theme.palette.text.secondary
+    },
+    drawer: {
+      flexBasis: drawerWidth
+    },
+    drawerPaper: {
+      width: drawerWidth
+    }
+    // sidebar: {
+    //     padding: theme.spacing(2),
+    //     textAlign: 'center',
+    //     // width: '100%',
+    //     // maxWidth: 360,
+    //     color: theme.palette.text.secondary,
+    // },
+    // nested: {
+    //     paddingLeft: theme.spacing(4),
+    // },
+  }
+})
